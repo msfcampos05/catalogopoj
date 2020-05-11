6ea9ca0df8f4f9983d9a84a58e7f32ae42ffbec9
Marcio <msfcampos05@gmail.com>
2020-05-11 10:35:32 -0300

    update

--- App/screens/Loading.js
+++ App/screens/Loading.js
@@ -1,1 +1,25 @@
-export default () => null;
+import React, { useState, useEffect } from 'react';
+import { View, StatusBar } from 'react-native';
+import Lottie from 'lottie-react-native';
+import dataloading from '../Components/loaders/home-loading.json';
+
+function Loading({ navigation }) {
+  
+  return (
+
+    <View style={{ flex: 1, justifyContent: 'space-around', alignItems: 'center',backgroundColor: '#9b58b6' }}>
+      <StatusBar
+        hidden={true}
+        translucent={false}
+        animated={true} 
+        backgroundColor='#9b58b6'
+      />
+      <Lottie source={dataloading} style={{ width: 450, height: 450 }} autoPlay loop />
+    </View>
+
+  );
+
+
+}
+
+export default Loading;
